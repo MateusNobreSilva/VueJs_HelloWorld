60f2ca95986a7f56cf01d88343f4e28b78ee57fc
Mateus_Nobre <mateusnobresilva@gmail.com>
2021-11-15 15:57:54 -0300

    primeiro commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,28 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <b-container>
+    <b-row align-v="center">
+      <b-col md="3">
+        <b-card title="Card Title">
+          <img class="mb-2" src="https://picsum.photos/600/300/?image=25" />
+          <b-card-text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </b-card-text>
+
+          <b-button variant="primary">Go somewhere</b-button>
+        </b-card>
+      </b-col>
+    </b-row>
+  </b-container>
+
+  <v-card elevation="2" shaped tile></v-card>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  name: "Home",
+  components: {},
+};
 </script>
